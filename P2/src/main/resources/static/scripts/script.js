ef3bdc653c2b0546c67976c073cbd2730debdb24
fluffb4ll <real.fluffball@gmail.com>
2024-04-14 21:45:22 +0500

    GET params WIP

--- P2/src/main/resources/static/scripts/script.js
+++ P2/src/main/resources/static/scripts/script.js
@@ -2,6 +2,61 @@ addUserBt.addEventListener("click", () => {
     addUser();
 });
 
+findByNameCB.addEventListener("change", () => {
+    if(findByNameCB.checked){
+        let el = document.createElement("input");
+        el.type = "text";
+        el.placeholder = "Имя";
+        el.id = "findByNameT";
+
+        document.getElementsByClassName("findByName")[0].appendChild(el);
+    }
+    else {
+        document.getElementsByClassName("findByName")[0].removeChild(document.getElementById("findByNameT"));
+    };
+});
+
+findByAgeCB.addEventListener("change", () => {
+    if(findByAgeCB.checked){
+        let el = document.createElement("input");
+        el.type = "number";
+        el.placeholder = "Возраст";
+        el.id = "findByAgeT";
+
+        document.getElementsByClassName("findByAge")[0].appendChild(el);
+    }
+    else {
+        document.getElementsByClassName("findByAge")[0].removeChild(document.getElementById("findByAgeT"));
+    };
+});
+
+sortFieldCB.addEventListener("change", () => {
+    if(sortFieldCB.checked){
+        let field = document.createElement("input");
+        field.type = "text";
+        field.placeholder = "Поле (name/age)";
+        field.id = "sortFieldT";
+
+        let order = document.createElement("input");
+        order.type = "checkbox";
+        order.id = "sortOrderCB";
+
+        let label = document.createElement("label");
+        label.htmlFor = "sortOrder";
+        label.innerText = "Порядок (asc/desc)";
+        label.id = "sortOrderL";
+
+        document.getElementsByClassName("sort")[0].appendChild(field);
+        document.getElementsByClassName("sort")[0].appendChild(order);
+        document.getElementsByClassName("sort")[0].appendChild(label);
+    }
+    else {
+        document.getElementsByClassName("sort")[0].removeChild(document.getElementById("sortFieldT"));
+        document.getElementsByClassName("sort")[0].removeChild(document.getElementById("sortOrderCB"));
+        document.getElementsByClassName("sort")[0].removeChild(document.getElementById("sortOrderL"));
+    };
+});
+
 getUsersBt.addEventListener("click", () => {
     getUserList();
 });
@@ -13,6 +68,8 @@ deleteUserBt.addEventListener("click", () => {
 editUserBt.addEventListener("click", () => {
     editUser();
 });
+
+
 
 function getUserList() {
     fetch("api/getUsers", {
